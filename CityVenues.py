322593f939b2204939c8d4d0d2539056c0889d54
Sanctus Gee <godwin.dsg@gmail.com>
2013-01-30 16:03:34 -0700

    Update CityVenues.py

--- CityVenues.py
+++ CityVenues.py
@@ -1,3 +1,38 @@
+#coding: utf-8
+# ----------------------------------------------------------------------------
+# CityVenues
+# Copyright (c) 2012 Godwin Effiong
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in
+# the documentation and/or other materials provided with the
+# distribution.
+# * Neither the name of pyprocessing nor the names of its
+# contributors may be used to endorse or promote products
+# derived from this software without specific prior written
+# permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# ----------------------------------------------------------------------------
+
 import io
 import os
 import codecs
@@ -119,7 +154,10 @@ def run_search():
                                          {"name": {"$blank":False}},
                                          {"cuisine": {"$blank":False}}                                         
                                          ]
-                                 }).limit(1)
+                                 })
+                                 # the Factual API allows a max of 20 rows per "Place"
+                                 # this can be changed to ".limit(20)"
+                                 .limit(1) 
        
        #explicitly specify the fields to return
        search = search.select("rating, tel, postcode, parking, seating_outdoor, category, cuisine, locality,"
